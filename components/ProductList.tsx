5870539b641444d98efe8b20a1454775e961699c
Shiuan Chou <im.cps0903@gmail.com>
2023-06-15 12:23:35 +0800

    新增 - Products Card

---
+++ components/ProductList.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import styles from '@/scss/_product.module.scss'
+import { ProductsInterface } from '@/types/product.interface';
+import AddToBagBtn from './AddToBagBtn';
+import Link from 'next/link';
+import { HiOutlineArrowSmallRight } from 'react-icons/hi2';
+import { useRouter } from 'next/navigation';
+
+function ProductCard({ props }: { props: ProductsInterface }) {
+  const router = useRouter();
+  return (
+    <article className={styles.productCard} onClick={() => router.push(`/products/${props.id}`)}>
+      <div className={styles.plantImg}>
+        <img src={`/images/${props.name}.png`} alt={props.name} />
+      </div>
+      <div className={styles.content}>
+        {props.monthlyNews && <span className={styles.news}>本月新品</span>}
+        <h2>{props.name}</h2>
+        <p className={styles.price}>NT$ {props.price}</p>
+        <AddToBagBtn id={props} />
+      </div>
+    </article>
+  )
+}
+
+export default function ProductList({ displayData, title, moreBtn }:
+  { displayData: ProductsInterface[], title: string, moreBtn: boolean }) {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.mainTitle}>{title}</h2>
+      <div className={styles.wrap}>
+        {displayData.map((data: ProductsInterface, index: number) => {
+          return (
+            <ProductCard props={data} key={index} />
+          )
+        })}
+      </div>
+      {moreBtn && <Link href={'/products'}><span className={styles.more}>更多日日盆栽<HiOutlineArrowSmallRight /></span></Link>}
+    </section>
+  )
+}
---
+++ types/product.interface.ts
@@ -0,0 +1,9 @@
+export interface ProductsInterface {
+    id: number,
+    name: string,
+    description: string,
+    imagePath: string,
+    price: number,
+    available: number,
+    monthlyNews: boolean,
+  }
