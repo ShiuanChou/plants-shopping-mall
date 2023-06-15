cb5c5cfd508f51c1915cf35371486c33d5e0e3d9
Shiuan Chou <im.cps0903@gmail.com>
2023-06-15 12:22:13 +0800

    調整 - layout

---
+++ components/layouts/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from '@/scss/layouts/_footer.module.scss'
+
+export default function Footer() {
+    return (
+        <footer className={styles.container}>
+            日日靜好 All rights reserved.
+        </footer>
+    )
+}
---
+++ components/layouts/Navbar.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import styles from '@/scss/layouts/_navbar.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import { SlBag } from "react-icons/sl";
+import Logo from '@/public/images/logo.png';
+
+export default function Navbar() {
+    return (
+        <nav className={styles.container}>
+            <div className={styles.logo}>
+                <Link href={'/'}><Image src={Logo} alt='logo' width={40} height={40} /></Link>
+            </div>
+            <div className={styles.function}>
+                <Link href={'/products'}>日日盆栽</Link>
+                <Link href={'/'}>運送方式</Link>
+                <div className={styles.shoppingBag}>
+                    <SlBag className={styles.icon} />
+                    <span className={styles.count}>0</span>
+                </div>
+            </div>
+        </nav>
+    )
+}
